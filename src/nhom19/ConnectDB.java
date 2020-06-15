d101c07844011e0026e614aa8ada1e751cb79f8c
luctran23 <anhvanluc1999@gmail.com>
2020-06-15 22:48:39 +0700

    Update

--- src/nhom19/ConnectDB.java
+++ src/nhom19/ConnectDB.java
@@ -15,7 +15,7 @@ public class ConnectDB {
     Connection cnn = null;
 
     String uRl = "jdbc:derby://localhost:1527/QLRuou";
-    String userName = "nhom20";
+    String userName = "nhom19";
     String pas = "1";
 
     
